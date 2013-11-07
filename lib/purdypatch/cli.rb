87b8ab5d9287dc184e1a9a075051ded6921dd25c
Nathan de Vries <nathan@atnan.com>
2013-11-06 23:57:10 -0800

    Move CLI code into purdypatch/cli.rb.
    Be a good citizen and don't throw junk into purdypatch.rb.

---
+++ lib/purdypatch/cli.rb
@@ -0,0 +1,44 @@
+require 'mail'
+require 'pathname'
+require 'purdypatch/version'
+require 'prettypatch/PrettyPatch'
+
+module PurdyPatch
+  class CLI
+    def self.start(*args)
+      if args.empty?
+        puts "purdypatch expects one or more files created via `git format-patch --attach [ <since> | <revision range> ]`."
+        exit
+      end
+
+      args.each do |arg|
+        patch_mail_path = Pathname.new(arg)
+        next unless patch_mail_path.exist?
+
+        mail = Mail.read(patch_mail_path)
+        parts = mail.parts.dup
+        purdified = false
+        parts.each_with_index do |part, i|
+          next unless part.attachment? && part.content_type =~ %r(text/x-patch)
+
+          purdy_part = Mail::Part.new do
+            content_type 'text/html; charset=UTF-8'
+            body PrettyPatch.prettify(part.body.decoded)
+          end
+          mail.parts.insert(i, purdy_part)
+          mail.header['X-Formatter'] = "PurdyPatch-#{PurdyPatch::VERSION}"
+          purdified = true
+        end
+
+        if purdified
+          purdy_patch_mail_path = patch_mail_path.sub_ext('.purdypatch')
+          File.open(purdy_patch_mail_path, File::CREAT | File::TRUNC | File::RDWR) do |f|
+            f.write mail.to_s
+          end
+          puts %Q(Purdified patch email saved to "#{purdy_patch_mail_path}".)
+        end
+
+      end
+    end
+  end
+end
