401b9c63354cbe400e064cc2728482377d10d231
Nathan de Vries <ndevries@apple.com>
2014-08-14 01:28:07 -0700

    Add part for notes and improve gitstat formatting
    Prepend an empty plain text part to the start of the mail body to make it easy
    to include some notes about the patch prior to sending it out for review.
    
    To distinguish the notes from the gitstat section, wrap the gitstat in pre
    tags within an HTML part, ensuring that the body is HTML escaped.

--- lib/purdypatch/cli.rb
+++ lib/purdypatch/cli.rb
@@ -1,3 +1,4 @@
+require 'cgi'
 require 'mail'
 require 'pathname'
 require 'purdypatch/version'
@@ -16,18 +17,29 @@ module PurdyPatch
         next unless patch_mail_path.exist?
 
         mail = Mail.read(patch_mail_path)
-        parts = mail.parts.dup
-        purdified = false
-        parts.each_with_index do |part, i|
-          next unless part.attachment? && part.content_type =~ %r(text/x-patch)
 
-          purdy_part = Mail::Part.new do
-            content_type 'text/html; charset=UTF-8'
-            body PrettyPatch.prettify(part.body.decoded)
+        purdified = false
+        mail.parts.dup.each_with_index do |part, i|
+          if part.content_type =~ %r(text/plain)
+            mail.parts[i] = Mail::Part.new do
+              content_type 'text/html; charset=UTF-8'
+              body "<html><body><pre>--\n#{ CGI::escapeHTML part.body.decoded }</pre></body></html>"
+            end
+
+          elsif part.attachment? && part.content_type =~ %r(text/x-patch)
+            purdy_part = Mail::Part.new do
+              content_type 'text/html; charset=UTF-8'
+              body PrettyPatch.prettify(part.body.decoded)
+            end
+
+            mail.parts.insert(i, purdy_part)
+            mail.header['X-Formatter'] = "PurdyPatch-#{PurdyPatch::VERSION}"
+            purdified = true
           end
-          mail.parts.insert(i, purdy_part)
-          mail.header['X-Formatter'] = "PurdyPatch-#{PurdyPatch::VERSION}"
-          purdified = true
+        end
+
+        mail.parts << Mail::Part.new do
+          content_type 'text/plain; charset=UTF-8'
         end
 
         if purdified
